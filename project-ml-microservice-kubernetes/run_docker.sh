38734a6bd162386fbbd302716bb89a167e2ca7ff
Ubuntu <ubuntu@ip-172-31-80-5.ec2.internal>
2022-07-21 13:05:15 +0000

    tache 2 ok

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,14 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=myapp:1.0 .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+
+# docker run -ti --rm -p 8000:80 --name my_app myapp:1.0 sh
+docker run -ti --rm -d -p 8000:80 --name my_app myapp:1.0
